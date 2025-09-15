c58e0a68e4d8f684f98d82048f900d2c40d5888e
Senadha Weerasooriya <IT25100964@my.sliit.lk>
2025-09-15 23:25:03 +0530

    created functions for part 1

---
+++ tictactoe.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void initializeBoard(){
+
+
+}
+
+void displayBoard(){
+
+}
+
+void acceptUserInput(){
+
+
+}
+
+void ValidateInput(){
+
+
+}
+
+void checkWin(){
+
+}
+
+void checkDraw(){
+
+
+}
+
+
+int main(){
+
+
+
+    return 0;
+}
+
